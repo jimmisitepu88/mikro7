1a695fcff33ba93e3ed176644f4e5b34ce1638f1
Jimmi Kardo Sitepu <32915206+jimmisitepu88@users.noreply.github.com>
2018-09-30 10:26:20 +0700

    add test fungsi
    add test fungsi, check all segemtn

--- mikro7.h
+++ mikro7.h
@@ -19,8 +19,10 @@ class mikro7
 	uint8_t bit_flag;
 
 	uint8_t decDigits[10] = {0b11101110,0b10001000,0b11000111,0b11001101,0b10101001,0b01101101,0b01101111,0b11001000,0b11101111,0b11101101};
-    uint16_t dt_temp;
+    //uint8_t decDigits[10] = {0b11000000,0b11111001,0b10100100,0b10110000,0b10011001,0b10010010,0b10000010,0b11111000,0b10000000,0b10010000};
+	uint16_t dt_temp;
     uint8_t satuan_POS1, puluhan_POS1, ratusan_POS1, ribuan_POS1, puluhanRibu_POS1,ratusanRibu_POS1;
+	uint8_t check_all;
 	
     
     public:
@@ -31,6 +33,7 @@ class mikro7
     void display(uint16_t POS1, bool flag);
     void off();
 	void on();
+	void test();
 };
 
 #endif
